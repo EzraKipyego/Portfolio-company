1b24299e07bf726cbe6005dc391aa4672acfd642
EZRA KIPYEGO <ezra.kipyego@student.moringaschool.com>
2026-05-08 18:50:26 +0300

    styled  layout and spacing consistency

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+
+    return (
+        <>
+            <footer className="text-center py-6 mt-8">
+                <p>Portfolio Platform 2026</p>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
