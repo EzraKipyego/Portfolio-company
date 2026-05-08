b4c46f43d9518a071b48910e3e6a31bb824cfed9
EZRA KIPYEGO <ezra.kipyego@student.moringaschool.com>
2026-05-08 18:47:10 +0300

    Improved layout and responsivness

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <nav className="w-full border-b border-gray-300 bg-white">
+
+            <div className="max-w-5xl mx-auto py-6 px-6">
+
+                <h1 className="text-4xl font-bold text-center">
+                    Projects showcase App
+                </h1>
+
+                <p className="text-center text-gray-600 mt-2">
+                    A portfolio website for web Application designed and developed
+                </p>
+
+            </div>
+
+        </nav>
+    );
+};
+
+export default NavBar;
